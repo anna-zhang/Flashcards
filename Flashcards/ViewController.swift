7dffbabd2dfabbbb5b48243c81c0ab837ddf8cce
Anna Zhang <annazhangg@yahoo.com>
2020-02-15 14:03:04 -0500

    Initial Commit

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by zhanglj on 2/15/20.
+//  Copyright © 2020 zhanganna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
