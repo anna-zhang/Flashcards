178604a16184cd6f9134c5113627253c4b5ae8ba
Anna Zhang <annazhangg@yahoo.com>
2020-02-21 11:17:13 -0500

    Simple Flashcards lab done

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var frontLabel: UIButton!
+    @IBOutlet weak var backLabel: UIButton!
+    
+    @IBOutlet weak var buttonOptionOne: UIButton!
+    @IBOutlet weak var buttonOptionTwo: UIButton!
+    @IBOutlet weak var buttonOptionThree: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        styleCards.layer.cornerRadius = 20.0
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        styleCards.layer.shadowRadius = 15.0
+        styleCards.layer.shadowOpacity = 0.2
+        
+        
+        
     }
 
-
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true
+        backLabel.isHidden = false
+    }
+    
+    @IBAction func backToQuestion(_ sender: Any) {
+        backLabel.isHidden = true
+        frontLabel.isHidden = false
+    }
+    @IBOutlet weak var styleCards: UIView!
 }
 
